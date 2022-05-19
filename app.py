713f76df6cc04c5e89b715ae2e8e268edaa26d55
Yanxu <yanxu.zheng@kogan.com.au>
2022-05-19 16:29:38 +1000

    init

---
+++ app.py
@@ -0,0 +1,15 @@
+class OrderCalculator:
+    def __init__(self):
+        pass
+
+
+    def get_price(self):
+        """Get price for Order."""
+        return "hello world"
+
+def main():
+    print(OrderCalculator().get_price())
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_order_calcuator.py
@@ -0,0 +1,6 @@
+from ..app import OrderCalculator
+
+def test_order_calcuator_get_price():
+    price = OrderCalculator().get_price()
+
+    assert price == "hello world"
